28bca49728eb9c0bb4f35e7355f89b43264b7b08
alibenslima <alibenslima@yahoo.fr>
2023-02-28 22:45:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Spinner } from "react-bootstrap";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const getUsers = () => {
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((response) => response.json())
+  //       .then((data) => setUsers(data))
+  //       .catch((err) => console.log(err));
+  //   };
+  //   getUsers();
+  //   setLoading(false);
+  // }, []);
+  // console.log(users);
+
+  useEffect(() => {
+    const searchusers = async () => {
+      try {
+        const resultat = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        console.log(resultat);
+        setUsers(resultat.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    searchusers();
+    setLoading(false);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+      {loading ? (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        <div>
+          {users.map((el) => (
+            <div key={el.id} style={{ border: "solid 2px" }}>
+              <h1 style={{ color: "blue " }}>{el.name}</h1>
+              <h1 style={{ color: "green " }}>{el.username}</h1>
+              <p>{el.email}</p>
+              <p>{el.address.city}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
